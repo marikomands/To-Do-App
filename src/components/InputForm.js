af54a42a93d54ab0d48b19d10e1f257f2b66bda6
marikomands <marikomands@gmail.com>
2023-06-22 14:35:23 +0100

    added handleChange and handleSubmit with unique ID

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const InputForm = () => {
+// function generateUniqueId() {
+//   return Math.random().toString(36).substr(2, 9);
+// }
+
+function generateUniqueId() {
+  return uuidv4();
+}
+
+const InputForm = ({ taskList, setTaskList }) => {
+  const [inputText, setInputText] = useState("");
+  console.log("🚀 ~ InputForm ~ inputText:", inputText);
+
+  const handleSubmit = (e) => {
+    console.log("a");
+    e.preventDefault();
+    setTaskList([...taskList, { text: inputText, id: generateUniqueId() }]);
+    setInputText("");
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
   return (
     <div className="inputForm">
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" value={inputText} />
         <button>
-          <i class="fa-solid fa-square-plus"></i>
+          <i className="fa-solid fa-square-plus"></i>
         </button>
       </form>
     </div>
