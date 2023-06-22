207c84ed0083c2e02141db631734be5369db1e8f
marikomands <marikomands@gmail.com>
2023-06-22 10:48:13 +0100

    created InputForm components

---
+++ src/components/InputForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InputForm = () => {
+  return (
+    <div className="inputForm">
+      <form>
+        <input type="text" />
+        <button>
+          <i class="fa-solid fa-square-plus"></i>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;
